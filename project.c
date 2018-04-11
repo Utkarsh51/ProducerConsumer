b43104fdf5781373c4100b92003852c7a7e7dd82
Utkarsh Pratap Singh <Utkarsh51@users.noreply.github.com>
2018-04-12 02:00:41 +0530

    Update project.c

--- project.c
+++ project.c
@@ -15,7 +15,21 @@ int buffer_index;
 pthread_mutex_t buffer_mutex;
 sem_t full_sem;  // will return 0 when buffer will be full  
 sem_t empty_sem; // will return 0 when  buffer is empty works like index for the buffer */ 
-int main(){
-{
-
+//method to put produced items in buffer
+void insertInBuffer(buffer_t value) {
+    if (bufferIndex < Size) {
+        buffer[bufferIndex++] = value;
+    } else {
+        printf("Buffer is Full can not produce\n");
+    }
 }
+//method to remove consumed items from buffer
+buffer_t deleteFromBuffer() {
+    if (bufferIndex > 0) {
+        return buffer[--bufferIndex]; 
+    } else {
+        printf("Buffer is empty Nothing to consume\n");
+    }
+    return 0;
+}
+
