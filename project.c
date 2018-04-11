8564ba9317da6f16321b7a50b6eb24f3d68e17f8
Utkarsh Pratap Singh <Utkarsh51@users.noreply.github.com>
2018-04-11 23:43:57 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,24 +1,21 @@
+//header files//
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
-int Shared_Buffer[500];
-void * produce()
+#include<stdlib.h>
+
+#define Size 5
+#define NoThreads 8
+#define ProducerLoops 10
+//Buffer Definition//
+typedef int buffer_t;
+buffer_t buffer[Size];
+int buffer_index;
+//semaphores and mutex//
+pthread_mutex_t buffer_mutex;
+sem_t full_sem;  // will return 0 when buffer will be full  
+sem_t empty_sem; // will return 0 when  buffer is empty works like index for the buffer */ 
+int main(){
 {
-printf("produce");
-}
-void * consume()
-{
-printf("consume);
-}
-void main(){
-{
-int i;
-int No_of_Producers;
-int No_of_Consumers;
-printf("Enter the number of producers");
-printf("Enter number of consumers");
-scanf("%d",&No_of_Producers);
-scanf("%d",&No_of_Consumers);
-for(i=0;i<No_of_Producers;i++){
-}
+
 }
